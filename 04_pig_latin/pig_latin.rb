75541fc63162b0189ac70f7b18b9854c366616af
Yash Anand <yashanand1910@gmail.com>
2016-12-13 18:15:05 +0530

    completed first 5

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
-#write your code here
+def transWord text
+	if text[0..1] == 'qu'
+		text + text.slice!(0..1) + 'ay'
+	elsif text =~ /\A[aeiou]/
+		text + 'ay'
+	elsif text[0] =~ /[^aeiou]/
+		if text[1..2] == 'qu'
+			text + text.slice!(0..2) + 'ay'
+		elsif text[1] =~ /[^aeiou]/
+			if text[2] =~ /[^aeiou]/
+				text + text.slice!(0..2) + 'ay'
+			else
+				text + text.slice!(0..1) + 'ay'
+			end
+		else
+			text + text.slice!(0) + 'ay'
+		end
+	end
+end
+def translate text
+	text = text.split ' '
+	text.length.times do |i|
+		text[i] = transWord text[i]
+	end
+	text.join ' '
+end
